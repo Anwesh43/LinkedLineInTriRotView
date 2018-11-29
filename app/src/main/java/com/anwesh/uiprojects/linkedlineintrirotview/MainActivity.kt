750b0c0e7397566af55e75b96d1ebaa27ca8a9c5
Anwesh43 <anweshthecool0@gmail.com>
2018-11-29 11:17:40 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlineintrirotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlineintrirotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlineintrirotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lineintrirotview.LineInTriRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineInTriRotView.create(this)
     }
 }

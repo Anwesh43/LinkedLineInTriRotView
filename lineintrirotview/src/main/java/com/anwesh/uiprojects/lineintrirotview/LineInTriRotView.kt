a9239ee7592c8213a3e715ec763daeb16abc6a6a
Anwesh43 <anweshthecool0@gmail.com>
2018-11-29 10:44:18 +0530

    adding libraries and defining constants

--- lineintrirotview/src/main/java/com/anwesh/uiprojects/lineintrirotview/LineInTriRotView.kt
+++ lineintrirotview/src/main/java/com/anwesh/uiprojects/lineintrirotview/LineInTriRotView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.lineintrirotview
 /**
  * Created by anweshmishra on 29/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val color : Int = Color.parseColor("#283593")
+val strokeFactor : Int = 90
+val sizeFactor : Int = 3

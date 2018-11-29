c0511a7f84b1459df51a4ff484814410b3b1c112
Anwesh43 <anweshthecool0@gmail.com>
2018-11-29 10:50:32 +0530

    adding Float and Int extension methods

--- lineintrirotview/src/main/java/com/anwesh/uiprojects/lineintrirotview/LineInTriRotView.kt
+++ lineintrirotview/src/main/java/com/anwesh/uiprojects/lineintrirotview/LineInTriRotView.kt
@@ -21,6 +21,16 @@ val color : Int = Color.parseColor("#283593")
 val strokeFactor : Int = 90
 val sizeFactor : Int = 3
 
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.getScaleFactor(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
 class LineInTriRotView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
